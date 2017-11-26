f950ca8edb10d88ec35f610b79b873c6609b34f6
Chaim - Behalf <chaim@behalf.com>
2017-11-26 14:28:51 +0200

    fix json

--- smashing_service/src/main/scala/com/tikal/antsmasher/actors/Game.scala
+++ smashing_service/src/main/scala/com/tikal/antsmasher/actors/Game.scala
@@ -1,30 +1,56 @@
 package com.tikal.antsmasher.actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorLogging}
 import com.tikal.antsmasher.SmashingServiceApp
+import com.tikal.antsmasher.actors.Game.{PauseGame, StartGame, StopGame}
 import com.tikal.antsmasher.domain.{Ant, HitType}
 import com.tikal.antsmasher.services.KafkaProducerService
 import org.json4s._
-import org.json4s.native.JsonMethods._
+
 
 import scala.collection.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class Game(val gameId: Integer) extends Actor {
+class Game(val gameId: Integer) extends Actor with ActorLogging {
   val kafkaProducer: KafkaProducerService = new KafkaProducerService(SmashingServiceApp.SMASH_TOPIC, context.system)
   val ants = new mutable.HashMap[Integer, Boolean]
 
   override def receive = {
+    case StartGame(gameId) => {
+      if (gameId == this.gameId)
+        context.become(activeGame)
+    }
+    case StopGame(gameId) => {
+      if (gameId == this.gameId)
+        context.become(nonActiveGame)
+    }
+    case PauseGame(gameId) => {
+      if (gameId == this.gameId)
+        context.become(nonActiveGame)
+    }
+  }
+
+
+  def nonActiveGame: Receive = {
+    case _ => {
+      log.warning(s"game $gameId is not active")
+    }
+  }
+
+  def activeGame: Receive = {
     case ant: Ant => {
       implicit val formats = DefaultFormats
+      //      val ant = parse(js).extract[Ant]
+
       val prevoiusHit = ants.get(ant.id)
       val antStatus =
         if (prevoiusHit.isDefined && ant.hitType == HitType.Smash) {
           Ant(ant.id, ant.gameId, ant.teamId, HitType.Hit)
         }
         else ant
-      //      val ant = parse(js).extract[Ant]
+
+      import org.json4s.native.JsonMethods._
       val document = render(Extraction.decompose(antStatus))
       val t = compact(document)
       Future {
--- smashing_service/src/main/scala/com/tikal/antsmasher/actors/Manager.scala
+++ smashing_service/src/main/scala/com/tikal/antsmasher/actors/Manager.scala
@@ -1,22 +1,23 @@
 package com.tikal.antsmasher.actors
 
 import akka.actor.{Actor, Props}
-import com.tikal.antsmasher.actors.Game.{StartGame, StopGame}
+import com.tikal.antsmasher.actors.Game.{PauseGame, StartGame, StopGame}
 import com.tikal.antsmasher.domain.{Ant, HitType}
 
-class Manager extends Actor{
+class Manager extends Actor {
   override def receive = {
     case StartGame(gameId) => {
       val antsGeneratorActor = context.actorOf(Props(new Game(gameId)), "Game" + gameId)
-          antsGeneratorActor ! Ant(1,2,3,HitType.Hit)
+      antsGeneratorActor ! Ant(1, 2, 3, HitType.Hit)
     }
     case StopGame(gameId) => {
-//      val antsGeneratorActor = context.actorOf(Props(new Game(gameId)), "Game" + gameId)
+      val game = context.actorSelection("/user/GameManager/Game"+gameId)
+      game ! StopGame(gameId)
+      //      val antsGeneratorActor = context.actorOf(Props(new Game(gameId)), "Game" + gameId)
     }
-    case _ =>{
-      System.out.print("unknown")
-
+    case PauseGame(gameId) => {
+      val game = context.actorSelection("/user/GameManager/Game"+gameId)
+      game ! PauseGame(gameId)
     }
-
   }
 }
